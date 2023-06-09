e4e099cbd90262dd91b78f4ce0a54061f93d1674
Victor Nieves <vnievess@gmail.com>
2023-06-09 05:59:36 -0400

    fixed typo and added stats to nmap

--- host_scan.sh
+++ host_scan.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # $1 format -> x.x.x.x/yy
-# TODO: check if the imput follows the expected format "x.x.x.x/yy"
+# TODO: check if the input follows the expected format "x.x.x.x/yy"
 # TODO: add "output dir" feature
 
 # Just some colors
@@ -10,7 +10,7 @@ GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 
 
-# Checking imput exists
+# Checking if input exists
 if [ "$#" -ne 1 ]
   then
   echo "Please provide only one argument"
@@ -21,7 +21,7 @@ fi
 # Checking if root (some issues depend on tool instalation, so using root
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit 1
 fi
 
 
@@ -38,13 +38,13 @@ TOTAL_HOSTS=$(wc -l live_hosts.txt | awk '{print $1}')
 printf "${GREEN}$TOTAL_HOSTS live hosts found! -> $PWD/live_hosts.txt ${NORMAL}\n"
 
 printf "${YELLOW}Doing fast nmap scan (only a few ports). This may take some time ${NORMAL}\n"
-sudo nmap -sS -Pn -p 21-23,80,443,445,8080,8443 -o nmap_fast_scan.txt -iL live_hosts.txt
+sudo nmap -sS -Pn -p 21-23,80,443,445,8080,8443 -o nmap_fast_scan.txt -iL live_hosts.txt --stats-every 1m
 printf "${GREEN}Nmap fast scan done! -> $PWD/nmap_fast_scan.txt ${NORMAL}\n"
 
 printf "${YELLOW}Doing full nmap scan. This may take some time $1 ${NORMAL}\n"
-sudo nmap -Pn -A -o nmap_full_scan.txt -iL live_hosts.txt
+sudo nmap -Pn -A -o nmap_full_scan.txt -iL live_hosts.txt --stats-every 1m
 printf "${GREEN}Nmap full scan done! -> $PWD/nmap_full_scan.txt ${NORMAL}\n"
 
 printf "${GREEN}Script complete! Happy hunting! ${NORMAL}\n"
 
-exit 
+exit 0
