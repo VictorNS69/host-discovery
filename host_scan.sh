70b9bd2c4509753c172c29078b9ecdeb6ed05667
Victor Nieves <vnievess@gmail.com>
2023-06-15 06:41:18 -0400

    updated structure with functions

--- host_scan.sh
+++ host_scan.sh
@@ -7,81 +7,135 @@ NORMAL=$(tput sgr0)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 
+#################
+### FUNCTIONS ###
+#################
 
-# Checking if input exists
-if [ "$#" -ne 2 ]
-  then
-  echo "Please provide only two arguments"
-  echo "Example usage: sudo ./host_scan.sh 192.168.0.0/24 /tmp/discovery"
-  exit 1
-fi
+Banner (){
+  echo "
+                   _                          
+ |_|  _   _ _|_   | \ o  _  _  _      _  ._   
+ | | (_) _>  |_   |_/ | _> (_ (_) \/ (/_ | \/ 
+                                           /                                                              
+"                                                       
+}
 
-# Checking output directory (2nd argument)
-OUTPUT_DIR="$(readlink -f "$2")"
-if [[ ! -d "$OUTPUT_DIR" ]]
-  then
-  echo "The directory \"$OUTPUT_DIR\" does not exist"
-  mkdir -p "$OUTPUT_DIR"
-  if [ $? -ne 0 ]
+CheckArguments (){
+  # Checking if input exists
+  # $1 is the number of arguments ($#)
+  if [ "$1" -ne 2 ]
     then
-    echo "Error creating "$OUTPUT_DIR" directory"
+    echo "Please provide only two arguments"
+    echo "Example usage: sudo ./host_scan.sh 192.168.0.0/24 /tmp/discovery"
+    exit 1
+  fi
+}
+
+CheckDir (){
+  # Checking output directory
+  # $1 is the directory
+  if [[ ! -d "$1" ]]
+    then
+    echo "The directory \"$1\" does not exist"
+    mkdir -p "$1"
+    if [ $? -ne 0 ]
+      then
+      echo "Error creating "$1" directory"
+      echo "Exiting"
+      exit 1
+    else
+      echo "Directory "$1" created"
+    fi
+  elif [[ ! -w "$1" ]]
+    then
+    echo "You have no write access in "$1""
     echo "Exiting"
     exit 1
   else
-    echo "Directory "$OUTPUT_DIR" created"
+    echo "Directory "$1" exists"
   fi
-elif [[ ! -w "$OUTPUT_DIR" ]]
-  then
-  echo "You have no write access in "$OUTPUT_DIR""
-  exit 1
-else
-  echo "Directory "$OUTPUT_DIR" exists"
-fi
+}
 
-# Checking if valid IP/mask
-n='([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
-m='([0-9]|[12][0-9]|3[012])'
+CheckValidIpMask (){
+  # Checking if valid IP/mask
+  # $1 is the IP/mask range
+  n='([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+  m='([0-9]|[12][0-9]|3[012])'
+  if [[ $1 =~ ^$n(\.$n){3}/$m$ ]]
+    then
+    printf "${GREEN}Target: $1 ${NORMAL}\n"
+  else
+    printf "$1 does not follow the expected expression x.x.x.x/[0-32] or is not a valid range"
+    echo "Exiting"
+    exit 1
+  fi
+}
+CheckRoot (){
+  # Checking if root (some issues depend on tool instalation, so using root)
+  if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    echo "Exiting"
+    exit 1
+  fi
+}
 
-if [[ $1 =~ ^$n(\.$n){3}/$m$ ]]
-  then
-  echo "valid IP range"
-else
-  printf "$1 does not follow the expected expression x.x.x.x/[0-32] or is not a valid range"
-fi
-exit
+Ping3Discovery (){
+  # Ping discovery
+  printf "${YELLOW}Doing ping3 scan. This may take some time ${NORMAL}\n"
+  for ip in $IP_LIST
+  do
+    sudo ping3 -c 1 $ip | grep -v "Timeout" | tee -a "$OUTPUT_DIR/ping3_sweep.txt"
+  done
+}
 
-# Checking if root (some issues depend on tool instalation, so using root)
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 1
-fi
+FastNmap (){
+  # Fast nmap scan web ports and few more
+  printf "${YELLOW}Doing fast nmap scan (only a few ports). This may take some time ${NORMAL}\n"
+  sudo nmap -sS -Pn -p 21-23,80,443,445,8080,8443 -T5 -o "$OUTPUT_DIR/nmap_fast_scan.txt" -iL "$OUTPUT_DIR/live_hosts.txt" --stats-every 1m
+  printf "${GREEN}Nmap fast scan done! -> \"$OUTPUT_DIR/nmap_fast_scan.txt\" ${NORMAL}\n"
+}
 
-printf "${GREEN}Target: $1 ${NORMAL}\n"
+FullNmap (){
+  # Nmap -A
+  printf "${YELLOW}Doing full nmap scan. This may take some time ${NORMAL}\n"
+  for ip in $(cat "$OUTPUT_DIR/live_hosts.txt")
+  do
+    sudo nmap -Pn -A -o "$OUTPUT_DIR/nmap_full_scan_${ip}.txt" --stats-every 1m ${ip}
+    printf "${GREEN}Nmap full scan for ${ip} done! -> \"$OUTPUT_DIR/nmap_full_scan_${ip}.txt\" ${NORMAL}\n"
+  done
+}
 
+##################
+###### MAIN ######
+##################
+
+Banner 
+OUTPUT_DIR=$(readlink -f "$2")
+# Checks
+CheckArguments $#
+CheckDir $OUTPUT_DIR
+CheckValidIpMask $1
+CheckRoot
+  
 # Transform IP/mask to all IPs in the range
 IP_LIST=$(nmap -sL -n $1 | awk '/Nmap scan report/{print $NF}')
-
-printf "${YELLOW}Doing ping3 scan. This may take some time ${NORMAL}\n"
-for ip in $IP_LIST
-do
-  sudo ping3 -c 1 $ip | grep -v "Timeout" | tee -a "$OUTPUT_DIR/ping3_sweep.txt"
-done
-
+  
+Ping3Discovery
+  
 cat "$OUTPUT_DIR/ping3_sweep.txt" | awk -F[\'] '{print $2}' > "$OUTPUT_DIR/live_hosts.txt"
 TOTAL_HOSTS=$(wc -l "$OUTPUT_DIR/live_hosts.txt" | awk '{print $1}')
+if [[ $TOTAL_HOSTS -eq 0 ]]
+then
+ printf "${GREEN}No live hosts found${NORMAL}\n"
+ #echo "Removing output directory (it is empty)"
+ #rm -rf "$OUTPUT_DIR"
+ echo "Exiting"
+ exit 0
+fi 
 printf "${GREEN}$TOTAL_HOSTS live hosts found! -> \"$OUTPUT_DIR/live_hosts.txt\" ${NORMAL}\n"
-
-printf "${YELLOW}Doing fast nmap scan (only a few ports). This may take some time ${NORMAL}\n"
-sudo nmap -sS -Pn -p 21-23,80,443,445,8080,8443 -T5 -o "$OUTPUT_DIR/nmap_fast_scan.txt" -iL "$OUTPUT_DIR/live_hosts.txt" --stats-every 1m
-printf "${GREEN}Nmap fast scan done! -> \"$OUTPUT_DIR/nmap_fast_scan.txt\" ${NORMAL}\n"
-
-printf "${YELLOW}Doing full nmap scan. This may take some time ${NORMAL}\n"
-for ip in $(cat "$OUTPUT_DIR/live_hosts.txt")
-do
-  sudo nmap -Pn -A -o "$OUTPUT_DIR/nmap_full_scan_${ip}.txt" --stats-every 1m ${ip}
-  printf "${GREEN}Nmap full scan for ${ip} done! -> \"$OUTPUT_DIR/nmap_full_scan_${ip}.txt\" ${NORMAL}\n"
-done
-
+  
+FastNmap
+ 
+FullNmap
 printf "${GREEN}Script complete! Happy hunting! ${NORMAL}\n"
-
 exit 0
